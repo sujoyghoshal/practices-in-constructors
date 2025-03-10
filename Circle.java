944ac8b5205843bfc76c75fa2c465efe92fc8df3
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-10 11:22:45 +0530

    [sujoy] desc: Implemented Circle class with constructor chaining.

---
+++ Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    double radius;
+
+    // Default constructor
+    public Circle() {
+        this(1.0); // Constructor chaining
+    }
+
+    // Parameterized constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
