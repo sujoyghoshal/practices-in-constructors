317d8b91b65668699caf932406d94fae4c671d8f
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-10 11:23:10 +0530

    [sujoy] desc: Developed HotelBooking class with multiple constructors.

---
+++ HotelBooking.java
@@ -0,0 +1,26 @@
+public class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    // Default constructor
+    public HotelBooking() {
+        this.guestName = "Guest";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    // Parameterized constructor
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+}
