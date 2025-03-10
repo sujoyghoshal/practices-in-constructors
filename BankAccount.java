f3498b9accb8e8230c95e5494e239016b1da9436
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-10 11:35:27 +0530

    [sujoy] desc: Created BankAccount and SavingsAccount classes to manage account details and balance.

---
+++ BankAccount.java
@@ -0,0 +1,30 @@
+class BankAccount {
+    public String accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(String accountNumber, String accountHolder, double balance) {
+        super(accountNumber, accountHolder, balance);
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolder);
+    }
+}
